abe6aaca67ee58dfc35ad7bdac3d697e1933ffc0
Nicolas Rossi <nick413rossi@gmail.com>
2023-03-04 12:41:06 -0500

    Updated visuals

--- portfolio-project/src/app/NavBar.tsx
+++ portfolio-project/src/app/NavBar.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import Image from 'next/image';
 import Link from "next/link";
-import { BsLinkedin, BsGithub } from 'react-icons/bs'
+import { BsLinkedin, BsGithub, BsCodeSquare } from 'react-icons/bs'
 
 import { useState } from "react";
 
@@ -13,11 +13,14 @@ export default function NavBar() {
 
         <main className="text-xs sm:text-xl">
             <section>
-                <nav className="py-5 px-4 mb-12 flex justify-between text-black-900 ">
+                <nav className="py-5 px-4 mb-12 flex justify-between text-white-900 ">
 
-                    <a href="#home" className=" px-10 hidden sm:block">Nicolas Rossi</a>
+                    <div className=" hidden sm:flex flex-row px-6">
+                    <BsCodeSquare size={'2rem'} color='#F64C72' />
+                    <a href="#home" className=" pl-4 text-2xl block text-white-900 font-bold">Nicolas Rossi</a>
+                    </div>
 
-                    <ul className="flex items-center">
+                    <ul className="flex items-center m-auto">
                         <li className="">
                             <a href="#about" className=" px-4">About</a>
                         </li>
@@ -32,7 +35,7 @@ export default function NavBar() {
                     <ul className="hidden sm:flex flex-row gap-2 items-center">
                         <li className="px-2 "><a href="https://www.linkedin.com/in/nicolas-d-rossi/" target="_blank"><BsLinkedin size={'2.75vh'} /></a></li>
                         <li className="px-2"><a href="https://github.com/N-Rossi" target="_blank"><BsGithub size={'2.75vh'}/></a></li>
-                        <li className="px-4 py-2 min-h-fit rounded-lg bg-black-900 text-white">
+                        <li className="px-4 py-2 min-h-fit rounded-lg bg-pink text-white-900 shadow-xl">
                             <a href="#" target="_blank">Resume</a>
                         </li>
                     </ul>
--- portfolio-project/src/app/projects/nationalParks/page.tsx
+++ portfolio-project/src/app/projects/nationalParks/page.tsx
@@ -29,7 +29,7 @@ export default function NationalPark() {
     };
   
     return (
-        <>
+        <div className=' bg-park-img bg-cover bg-center text-white-900'>
             <section className='h-10v w-screen pt-6'>
                 <nav className='flex flex-row justify-between px-10'>
                     <Link className=' font-bold' href={'/#projects'}><IoMdArrowRoundBack size={'2rem'} color={'black-900'} /></Link>
@@ -38,7 +38,7 @@ export default function NationalPark() {
                         <ul className="hidden sm:flex flex-row gap-2 items-center">
                             <li className="px-2 "><a href="https://www.linkedin.com/in/nicolas-d-rossi/" target="_blank"><BsLinkedin size={'2.75vh'} /></a></li>
                             <li className="px-2"><a href="https://github.com/N-Rossi" target="_blank"><BsGithub size={'2.75vh'}/></a></li>
-                            <li className="px-2 py-2 min-h-fit rounded-lg bg-black-900 text-white">
+                            <li className="px-2 py-2 min-h-fit rounded-lg bg-purple-300 text-white-900 ">
                                 <a href="#" target="_blank">Resume</a>
                             </li>
                         </ul>
@@ -46,27 +46,27 @@ export default function NationalPark() {
                 </nav>
             </section>
             <section className=' h-90v w-[100vw] p-6 flex justify-center items-center'>
-                <div className=' bg-black-900 rounded-2xl p-6 grid grid-rows-2 grid-cols-1 md:grid-rows-1 md:grid-cols-3 gap-4 h-[90%] md:h-[70%] w-11/12 md:w-[70%] shadow-lg'>
+                <div className='   bg-parks bg-cover bg-no-repeat bg-center rounded-2xl p-6 grid grid-rows-2 grid-cols-1 md:grid-rows-1 md:grid-cols-3 gap-4 h-[90%] md:h-[70%] w-11/12 md:w-[70%] shadow-xl'>
                     <div className=' justify-center md:justify-start flex flex-col tile'>
-                        <h2 className=' text-xl md:text-5xl text-white font-bold '> {project.title} </h2>
+                        <h2 className=' text-xl md:text-5xl text-blue-700  font-bold '> {project.title} </h2>
                         <br />
-                        <p className='py-4 text-white text-l flex-wrap'> {project.content} </p>
+                        <p className='py-4 text-blue-500  text-l flex-wrap'> {project.content} </p>
                         
                         <ul className="flex flex-row py-4 gap-6 justify-start">
-                            <li className="px-2 py-2 min-h-fit rounded-lg font-bold "><a href="https://nationalparks-next.vercel.app/" target="_blank"><BsLink size="1.5rem" color='white' /></a></li>
-                            <li className="px-2 py-2 min-h-fit rounded-lg font-bold"><a href="https://github.com/N-Rossi/nationalparks-next" target="_blank"><BsGithub size="1.5rem" color='white' /></a></li>
+                            <li className="px-2 py-2 min-h-fit rounded-lg font-bold "><a href="https://nationalparks-next.vercel.app/" target="_blank"><BsLink size="1.5rem" color='#2F2FA2' /></a></li>
+                            <li className="px-2 py-2 min-h-fit rounded-lg font-bold"><a href="https://github.com/N-Rossi/nationalparks-next" target="_blank"><BsGithub size="1.5rem" color='#2F2FA2' /></a></li>
                         </ul>
                     </div>
                     <div className=' flex flex-col m-auto tile row-start-2 md:col-start-2 md:col-end-4'>
                         <img className=' rounded-lg' src={project.images[currentPage]} alt={`Page ${currentPage}`} />
                         <div className='  p-4 flex flex-row justify-center'>
-                            <button className='pr-6' onClick={prevPage}><BsArrowLeftCircle size="2rem" color='white' /></button>
-                            <span className='text-white my-auto'>{`Page    ${currentPage + 1}  of  ${project.images.length}`}</span>
-                            <button className='pl-6' onClick={nextPage}><BsArrowRightCircle size="2rem" color='white' /></button>
+                            <button className='pr-6' onClick={prevPage}><BsArrowLeftCircle size="2rem" color='#2F2FA2' /></button>
+                            <span className='text-blue-700 my-auto'>{`Page    ${currentPage + 1}  of  ${project.images.length}`}</span>
+                            <button className='pl-6' onClick={nextPage}><BsArrowRightCircle size="2rem" color='#2F2FA2' /></button>
                         </div>
                     </div>
                 </div>
             </section>
-        </>
+        </div>
     );
 }
